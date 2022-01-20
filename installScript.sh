de7b0b92a99968462befb5c6b4e8bf44463a9964
Alexandre MONSCHEIN <alexandre.monschein@bedrockstreaming.com>
2022-01-20 15:45:27 +0100

    edit

--- installScript.sh
+++ installScript.sh
@@ -1,13 +1,14 @@
-apt install wget build-essential checkinstall  
+apt install wget -y
+apt install wget build-essential checkinstall -y
 apt install libreadline-gplv2-dev libncursesw5-dev libssl-dev \
-    libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev 
+    libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev -y
 cd /opt 
-wget https://www.python.org/ftp/python/3.9.6/Python-3.9.6.tgz 
+wget https://www.python.org/ftp/python/3.9.6/Python-3.9.6.tgz -y
 tar xzf Python-3.9.6.tgz 
 cd Python-3.9.6 
-./configure --enable-optimizations
-make altinstall
+./configure --enable-optimizations -y
+make altinstall -y
 rm -f /opt/Python-3.9.6.tgz
-pip3.9 install -U spacy
-pip3.9 install -U pandas
-python3.9 -m spacy download fr_core_news_lg
+pip3.9 install -U spacy -y
+pip3.9 install -U pandas -y
+python3.9 -m spacy download fr_core_news_lg -y
